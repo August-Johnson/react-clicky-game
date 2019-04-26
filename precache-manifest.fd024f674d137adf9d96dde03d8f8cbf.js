88358c2ff01d7da972fa3bc3a8c204b8ce4d39b0
August Johnson <gusjohnson98@gmail.com>
2019-04-25 19:41:09 -0500

    Updates

---
+++ precache-manifest.fd024f674d137adf9d96dde03d8f8cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "061e75ec561dd9691103f440bde79367",
+    "url": "/react-clicky-game/static/media/header-background.061e75ec.jpeg"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  },
+  {
+    "revision": "9a99426dfdb96d3f3fff",
+    "url": "/react-clicky-game/static/js/main.ea369f31.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/react-clicky-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "9a99426dfdb96d3f3fff",
+    "url": "/react-clicky-game/static/css/main.ac0dfaa3.chunk.css"
+  },
+  {
+    "revision": "f2753be697550f0eb0e910f9b040e445",
+    "url": "/react-clicky-game/index.html"
+  }
+];
